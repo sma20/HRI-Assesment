debd025e611bfe9d37d6420fd9e9694819fe881f
Arthidevi Balavignesh <ab237@hw.ac.uk>
2019-11-23 00:43:35 +0000

    failure emotion added

--- functions/emotions.py
+++ functions/emotions.py
@@ -8,3 +8,17 @@ def emot_game_success(robot):
     robot.say_text(textSay, voice_pitch=1.0, duration_scalar=1.0).wait_for_completed()
     robot.play_anim_trigger(cozmo.anim.Triggers.CodeLabWin).wait_for_completed()
 
+def emot_game_failure(robot):
+    textSay = "Sorry you lost the game."
+    robot.say_text(textSay, voice_pitch=2.0, duration_scalar=0.8).wait_for_completed()
+    robot.play_anim_trigger(cozmo.anim.Triggers.CodeLabBored).wait_for_completed()
+    head_action = robot.set_head_angle(cozmo.robot.MAX_HEAD_ANGLE, in_parallel=True)
+    head_action.wait_for_completed()
+    textSay = "No worries Try again"
+    robot.say_text(textSay, voice_pitch=2.0, duration_scalar=0.8).wait_for_completed()
+
+
+
+
+
+cozmo.run_program(emot_game_success, use_viewer=False)
